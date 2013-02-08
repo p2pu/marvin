fce80fdda96273927c9974033e2154d38ee03494
Dirk Uys <dirkcuys@gmail.com>
2013-02-08 14:02:19 +0200

    backup credentials config and fixes to scripts

--- lernanta/files/backup-media.sh
+++ lernanta/files/backup-media.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 BACKUP_PATH=/opt/backup/media
 LOG_HEADER="Lernanta_media_backup"
 TIMESTAMP=`date -u +"%Y-%m-%d"`
@@ -7,12 +7,12 @@ source /etc/backup-credentials.sh
 
 /usr/bin/env logger -t $LOG_HEADER "Starting dump"
 
-DST_FILE=$BACKUP_PATH/media.tar.gz
+DST_FILE=${BACKUP_PATH}/media.tar.gz
 
 /usr/bin/env tar -czf $DST_FILE -C /opt/lernanta/lernanta media/
 
 #upload dumped file to amazon S3
-/opt/aws_venv/bin/python /opt/active-memory/upload_rotate.py S3_BUCKET S3_PREFIX ${DST_FILE}
+/opt/aws_venv/bin/python /opt/active-memory/upload_rotate.py $S3_BUCKET $S3_PREFIX $DST_FILE
 if [ $? -ne 0 ]
 then
   /usr/bin/env logger -t $LOG_HEADER "upload to Amazon S3 failed, keeping file!"
--- lernanta/files/backup-mysql.sh
+++ lernanta/files/backup-mysql.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 BACKUP_PATH=/opt/backup/mysql
 LOG_HEADER="MySQL_Backups"
 TIMESTAMP=`date -u +"%Y-%m-%d"`
@@ -7,8 +7,8 @@ source /etc/backup-credentials.sh
 
 /usr/bin/env logger -t $LOG_HEADER "Starting dump"
 
-DST_FILE=$BACKUP_PATH/mysql_full.sql
-/usr/bin/env mysqldump -A -u$USER -p$PASWD > $DST_FILE
+DST_FILE=${BACKUP_PATH}/mysql_full.sql
+/usr/bin/env mysqldump -A -u$DB_USER -p$DB_PASS > $DST_FILE
 if [ $? -ne 0 ]
 then
   /usr/bin/env logger -t $LOG_HEADER "mysqldump failed with result: $?"
@@ -19,11 +19,11 @@ fi
 /usr/bin/env gzip -qf $DST_FILE
 
 #upload dumped file to amazon S3
-/opt/aws_venv/bin/python /opt/active-memory/upload_rotate.py -b S3_BUCKET -k S3_PREFIX ${DST_FILE}.gz
+/opt/aws_venv/bin/python /opt/active-memory/upload_rotate.py $S3_BUCKET $S3_PREFIX ${DST_FILE}.gz
 if [ $? -ne 0 ]
 then
   /usr/bin/env logger -t $LOG_HEADER "upload to Amazon S3 failed, keeping file!"
-  /bin/mv ${DST_FILE}.gz $BACKUP_PATH/mysql_full-${TIMESTAMP}.sql.gz
+  /bin/mv ${DST_FILE}.gz ${BACKUP_PATH}/mysql_full-${TIMESTAMP}.sql.gz
   #TODO send email to sysadmin
   exit
 fi

2db64ed6edec7bca757efb8801de3d1b3989b30c
Dirk Uys <dirkcuys@gmail.com>
2021-07-02 14:37:26 +0200

    Update monitor role to run a daily check for running containers

--- roles/monitor/files/p2pu-netwell/checks.py
+++ roles/monitor/files/p2pu-netwell/checks.py
@@ -12,3 +12,22 @@ Port('learningcircles.p2pu.org', 443).ssl_valid_for(days=10)
 Path('/').free_space(gb=2)
 #Path('/var/log/syslog').modified_within(hours=1)
 #Repo('/home/deploy/src/project').is_clean()
+
+import docker
+import os
+DOMAIN = os.environ.get('DOMAIN')
+client = docker.from_env()
+containers = [c.name for c in client.containers.list()]
+expected_container = [
+    'p2pu-nginx',
+    f'{DOMAIN}-celerybeat',
+    f'{DOMAIN}-celery',
+    f'{DOMAIN}',
+    'nginx-letsencrypt-companion',
+    'learningcircles-rabbitmq',
+    'p2pu-postgres',
+    'fluentd',
+]
+for c in expected_container:
+    oknotok = "OK" if c in containers else "ERROR"
+    print(f'Checking that docker container {c} is running: {oknotok}')

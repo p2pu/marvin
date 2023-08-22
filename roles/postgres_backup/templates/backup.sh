346d1a69ebd619ffbc4438f390b183474d515efa
Dirk Uys <dirkcuys@gmail.com>
2023-08-22 13:37:03 +0200

    Update postgres backup script to remove uploaded files from disk. Closes #26.

--- roles/postgres_backup/templates/backup.sh
+++ roles/postgres_backup/templates/backup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+set -e
 BACKUP_PATH="/var/docker-volumes/etherpad/backups"
 docker exec -i {{POSTGRES_CONTAINER}} pg_dump -Fc -U {{PG_ADMIN_USER}} -w {{DB_NAME}} > $BACKUP_PATH/{{DB_NAME}}-$(date -u +"%Y-%m-%d").dump
 ln -f $BACKUP_PATH/{{DB_NAME}}-$(date -u +"%Y-%m-%d").dump $BACKUP_PATH/{{DB_NAME}}.dump
 
 docker run -i --rm -w /var/backup --env-file /root/backup.env  -v $BACKUP_PATH:/var/backup python:3 bash -c "pip install s3-backup-rotate && upload_rotate.py {{BACKUP_BUCKET_NAME}} {{BACKUP_KEY_PREFIX}} /var/backup/{{DB_NAME}}.dump"
+
+rm $BACKUP_PATH/*.dump
